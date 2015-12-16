dfe7ff14219e500a237da6bd2630589119be1db4
绮ゅ嘲 <yuefeng@yuefengdemacbook-air.local>
2015-12-16 21:19:01 +0800

    Initial Commit

---
+++ ShoppingProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShoppingProject
+//
+//  Created by 粤峰 on 15/12/16.
+//  Copyright © 2015年 郑永能. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
